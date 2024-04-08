5103c821c928421a5b4e2fa7a298de8aab962ea7
ilyas <muhammadilyas827827@gmail.com>
2024-04-09 02:28:29 +0530

    colors fixed

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const whiteColor = Colors.white;
+  static const primaryColor = Color.fromRGBO(116, 15, 46, 1);
+  static const secondaryColor = Color.fromRGBO(136, 48, 64, 1);
 }
